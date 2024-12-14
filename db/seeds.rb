a212157c9ce6c591ec006e465650371e504784f2
Nur Meilinda <linda.nurmeilinda@gmail.com>
2024-12-14 22:50:00 +0700

    add seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,52 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Clearing existing data..."
+User.destroy_all
+SleepRecord.destroy_all
+Follow.destroy_all
+
+puts "Creating users..."
+users = []
+50.times do
+  users << User.create!(
+    name: Faker::Name.name,
+  )
+end
+
+puts "Creating sleep records..."
+users.each do |user|
+  5.times do
+    bedtime = Faker::Time.between(from: 30.days.ago, to: 1.days.ago)
+
+    sleep_record = SleepRecord.create!(
+      user: user,
+      bed_time: bedtime
+    )
+
+    sleep_duration = rand(6..10).hours
+
+    wake_time = bedtime + sleep_duration.to_i.seconds
+    sleep_record.update!(
+      wake_time: wake_time
+    )
+  end
+end
+
+puts "Creating follow relationships..."
+users.each do |user|
+  follow_count = rand(1..3)
+  users_to_follow = users.reject { |u| u == user }.sample(follow_count)
+
+  users_to_follow.each do |followed_user|
+    Follow.create!(
+      follower: user,
+      followed: followed_user
+    )
+  end
+end
+
+puts "Seed data created successfully!"
+
+ActiveRecord::Base.connection.execute(
+  "REFRESH MATERIALIZED VIEW weekly_sleep_records_summary"
+)
+
+puts "REFRESH MATERIALIZED VIEW weekly_sleep_records_summary successfully!"
