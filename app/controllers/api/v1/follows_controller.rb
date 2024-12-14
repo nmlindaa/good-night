360e6b441a11f85d672f813fea640b4210adbe49
Nur Meilinda <linda.nurmeilinda@gmail.com>
2024-12-14 16:47:46 +0700

    fix params typo

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class FollowsController < ApplicationController
       def follow
-        follow = Follow.follow(follow_params[:follower_id], follow_params[:following_id])
+        follow = Follow.follow(follow_params[:follower_id], follow_params[:followed_id])
         if follow.success?
           render json: { message: "Success" }, status: :ok
         else
@@ -13,7 +13,7 @@ module Api
       end
 
       def unfollow
-        unfollow = Follow.unfollow(follow_params[:follower_id], follow_params[:following_id])
+        unfollow = Follow.unfollow(follow_params[:follower_id], follow_params[:followed_id])
         if unfollow.success?
           render json: { message: "Success" }, status: :ok
         else
